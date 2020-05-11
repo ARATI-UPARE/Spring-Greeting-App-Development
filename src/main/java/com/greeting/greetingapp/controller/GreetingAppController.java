cd21e79be7343fc4c6ab9298cae6902b005a5d48
ARATI-UPARE <aratiupare@gmail.com>
2020-05-11 20:26:53 +0530

    ADDED: Ability for Greeting App to Give Greeting Message with UserData

--- src/main/java/com/greeting/greetingapp/controller/GreetingAppController.java
+++ src/main/java/com/greeting/greetingapp/controller/GreetingAppController.java
@@ -6,34 +6,41 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
+
 @RestController
 public class GreetingAppController {
 
     @Autowired
     GreetingAppService greetingAppService;
 
-    private String message ;
-    public String getMessage(String name) {
-        return this.message = "Hello! Welcome to Greetings Application "+name;
-    }
-
     @GetMapping("/greetings")
     public ResponseEntity getGreeting(){
+
         return new ResponseEntity(greetingAppService.getMessage(), HttpStatus.OK);
     }
 
-    @PostMapping("/greetings/{name}")
-    public ResponseEntity postGreeting(@PathVariable String name){
-        return new ResponseEntity(getMessage(name),HttpStatus.CREATED);
-    }
-
     @PutMapping("/greetings/update/{name}")
-    public ResponseEntity putGreetings(@PathVariable String name){
-        return new ResponseEntity(getMessage(name),HttpStatus.ACCEPTED);
+    public ResponseEntity updateGreetings(@PathVariable String name){
+        return new ResponseEntity(greetingAppService.getMessage(name),HttpStatus.ACCEPTED);
     }
 
     @DeleteMapping("greetings/delete/{name}")
     public ResponseEntity deleteGreeting(@PathVariable String name){
-        return new ResponseEntity(getMessage(name)+"Deleted", HttpStatus.ACCEPTED);
+        return new ResponseEntity(greetingAppService.getMessage(name)+"  Deleted", HttpStatus.ACCEPTED);
     }
+
+    @PostMapping("/greetings/userdata/{name}")
+    public ResponseEntity postGreeting(@PathVariable String name) {
+        return new ResponseEntity(greetingAppService.getMessage(name), HttpStatus.CREATED);
+
+    }
+
+    @PostMapping("/greetings/userdata/{firstName}/{lastName}")
+    public ResponseEntity postGreeting(@PathVariable String firstName,
+                                         @PathVariable String lastName) {
+            return new ResponseEntity(greetingAppService.userData(firstName, lastName), HttpStatus.CREATED);
+
+    }
+
 }
--- src/main/java/com/greeting/greetingapp/service/GreetingAppService.java
+++ src/main/java/com/greeting/greetingapp/service/GreetingAppService.java
@@ -1,11 +1,27 @@
 package com.greeting.greetingapp.service;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingAppService {
 
+    private String message;
+    private String firstName;
+    private String lastName;
+
     public String getMessage() {
-        return "Hello World!";
+
+        return "Hello World !";
+    }
+
+    public String getMessage(String name)
+    {
+        return this.message = "Hello ! "+name;
+    }
+
+    public String userData(String firstName, String lastName) {
+
+            return ("Hello ! " + firstName + " " + lastName);
     }
 }
